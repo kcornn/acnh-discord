42d9726ec55776cdd7bd638974467824cc8bcc75
Kali Cornn <kcornn@stanford.edu>
2020-06-20 16:42:42 -0700

    added readme and added villager command

---
+++ README.md
@@ -0,0 +1,9 @@
+# acnh-bot
+
+## Setup:
+
+Run `npm install` to install dependencies.
+
+Update `token` in `config.json` with the bot's `bot token`.
+
+Start the bot with `npm start`.
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const request = require('request');
+const cheerio = require('cheerio');
 const client = new Discord.Client();
 const config = require('./config.json');
 const prefix = config.prefix;
@@ -11,8 +13,50 @@ client.on("message", message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).split(' ');
 	const command = args.shift().toLowerCase();
+	console.log(args, command)
 	if (command === "villager") {
-		message.channel.send("hi");
+		const villagerName = args[0];
+		let picExists = false;
+		let villagerImgSrc;
+		let villagerEmbed = new Discord.MessageEmbed().setTitle(villagerName);
+		const link2 = `https://animalcrossing.fandom.com/wiki/${villagerName}_(villager)`;
+		request(link2, (err, res, body) => {
+			if (err) {
+				return message.channel.send(`There was an error getting info on ${villagerName}!`);
+			}
+			const $ = cheerio.load(body);
+			const villagerImgSrcRaw = $('figure').first().find('img').attr('src');
+			console.log("first", villagerImgSrcRaw)
+			if (villagerImgSrcRaw === undefined) {
+				return;
+			} else {
+				picExists = true;
+				villagerImgSrc = villagerImgSrcRaw.split('/').slice(0,8).join('/');
+				villagerEmbed = villagerEmbed.setThumbnail(villagerImgSrc);
+				// console.log($('figure').first().find('img').attr('src'))
+				message.channel.send(villagerEmbed);
+			}
+		});
+		const link = `https://animalcrossing.fandom.com/wiki/${villagerName}`;
+		request(link, (err, res, body) => {
+			if (err) {
+				return message.channel.send(`There was an error getting info on ${villagerName}!`);
+			}
+			const $ = cheerio.load(body);
+			const villagerImgSrcRaw = $('figure').first().find('img').attr('src');
+			console.log("second", villagerImgSrcRaw)
+			if (villagerImgSrcRaw === undefined && !picExists) {
+				return message.channel.send(`No picture of ${villagerName}!`);
+			} else if (picExists) {
+				return;
+			} else {
+				picExists = true;
+				villagerImgSrc = villagerImgSrcRaw.split('/').slice(0,8).join('/');
+				console.log(villagerImgSrc)
+				villagerEmbed = villagerEmbed.setThumbnail(villagerImgSrc);
+				message.channel.send(villagerEmbed);
+			}
+		});
 	}
 });
 
