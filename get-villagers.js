0dbd568a80e143eeb6c1c9f7d9c2c2a67aeec56e
Kali Cornn <kcornn@stanford.edu>
2020-06-23 01:06:26 -0700

    added comments to get-villagers file

--- get-villagers.js
+++ get-villagers.js
@@ -4,6 +4,8 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 let villagerList = [];
 const link = "https://animalcrossing.fandom.com/wiki/Villager_list_(New_Horizons)";
+
+// write all villager names to a json file - more villager data may be added in the future
 request(link, (err, res, body) => {
 	if (err) {
 		return message.channel.send(`There was an error getting info on ${villagerName}!`);
@@ -13,9 +15,9 @@ request(link, (err, res, body) => {
 	$('b').each(function (i, elem) {
 		villagerList[i] = $(this).text();
 	});
-	villagerList.shift();
-	// villagerName = villagerList[randInt].toLowerCase();
 
+	villagerList.shift(); // remove nonvillager name
+	
 	fs.writeFile("villagers.json", JSON.stringify(villagerList), function(err) {
 	    if (err) {
 	        console.log(err);
