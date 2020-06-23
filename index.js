5509aa4fc44ae24fb42a20451a7d084991c73132
Kevin Cua <kdizoncua@mail.sfsu.edu>
2020-06-22 18:06:32 -0700

    added guess villager game

--- index.js
+++ index.js
@@ -4,60 +4,188 @@ const cheerio = require('cheerio');
 const client = new Discord.Client();
 const config = require('./config.json');
 const prefix = config.prefix;
+const fs = require('fs');
+let villagerList = [];
+let villagerName;
+let guessOn = false;
 
 client.once('ready', () => {
 	console.log('Ready!');
+
+	const link = `https://animalcrossing.fandom.com/wiki/Villager_list_(New_Horizons)`;
+	request(link, (err, res, body) => {
+		if (err) {
+			return message.channel.send(`There was an error getting info on ${villagerName}!`);
+		}
+		const $ = cheerio.load(body);
+
+		$('b').each(function (i, elem) {
+			villagerList[i] = $(this).text();
+		});
+
+		villagerList.shift();
+		console.log(villagerList.length);
+
+	});
 });
 
+//get random villager
+function getRandomVillager(){
+	randInt = Math.floor(Math.random() * 392);
+	return villagerList[randInt];
+}
+
+//get villager picture
+
+function getVillagerPicture(villagerName, showName, message){
+	let picExists = false;
+	let villagerImgSrc;
+	let villagerEmbed;
+	if(showName){
+		villagerEmbed = new Discord.MessageEmbed().setTitle(villagerName);
+	}
+	else{
+		villagerEmbed = new Discord.MessageEmbed().setTitle('??');
+	}
+	const link2 = `https://animalcrossing.fandom.com/wiki/${villagerName}_(villager)`;
+	request(link2, (err, res, body) => {
+		if (err) {
+			return message.channel.send(`There was an error getting info on ${villagerName}!`);
+		}
+		const $ = cheerio.load(body);
+		const villagerImgSrcRaw = $('figure').first().find('img').attr('src');
+		console.log("first", villagerImgSrcRaw)
+		if (villagerImgSrcRaw === undefined) {
+			return;
+		} else {
+			picExists = true;
+			villagerImgSrc = villagerImgSrcRaw.split('/').slice(0, 8).join('/');
+			villagerEmbed = villagerEmbed.setThumbnail(villagerImgSrc);
+			// console.log($('figure').first().find('img').attr('src'))
+			message.channel.send(villagerEmbed);
+		}
+	});
+	const link = `https://animalcrossing.fandom.com/wiki/${villagerName}`;
+	request(link, (err, res, body) => {
+		if (err) {
+			return message.channel.send(`There was an error getting info on ${villagerName}!`);
+		}
+		const $ = cheerio.load(body);
+		const villagerImgSrcRaw = $('figure').first().find('img').attr('src');
+		console.log("second", villagerImgSrcRaw)
+		if (villagerImgSrcRaw === undefined && !picExists) {
+			return message.channel.send(`No picture of ${villagerName}!`);
+		} else if (picExists) {
+			return;
+		} else {
+			picExists = true;
+			villagerImgSrc = villagerImgSrcRaw.split('/').slice(0, 8).join('/');
+			console.log(villagerImgSrc)
+			villagerEmbed = villagerEmbed.setThumbnail(villagerImgSrc);
+			message.channel.send(villagerEmbed);
+		}
+	});
+}
+//commands load
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./commands/', (err, files) => {
+	if (err) console.error(err);
+	
+	files.forEach(f => {
+		let props = require(`./commands/${f}`);
+		// log(`Command Loaded! ${props.help.name}`);
+		client.commands.set(props.help.name, props);
+		props.conf.aliases.forEach(alias => {
+			client.aliases.set(alias, props.help.name);
+		});
+	});
+});
+
+//command reload
+client.reload = command => {
+	return new Promise((resolve, reject) => {
+		try {
+			delete require.cache[require.resolve(`./commands/${command}`)];
+			let cmd = require(`./commands/${command}`);
+			client.commands.delete(command);
+			client.aliases.forEach((cmd, alias) => {
+				if (cmd === command) client.aliases.delete(alias);
+			});
+			client.commands.set(command, cmd);
+			cmd.conf.aliases.forEach(alias => {
+				client.aliases.set(alias, cmd.help.name);
+			});
+			resolve();
+		} catch (e) {
+			reject(e);
+		}
+	});
+};
+
 client.on("message", message => {
+
+	if (guessOn) {
+		if(wrongAnswer){
+			if (message.author.bot) return;
+			const args = message.content;
+			userInput = args.toLowerCase();
+			console.log(userInput);
+			if (userInput){
+				console.log(villagerName);
+				if (userInput === villagerName) {
+					message.channel.send("that's correct! :partying_face: :partying_face:");
+					wrongAnswer = false;
+					guessOn = false;
+				}
+				else{
+					message.channel.send("that's incorrect! try again");
+				}
+			}
+	
+		}
+	}
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).split(' ');
 	const command = args.shift().toLowerCase();
 	console.log(args, command)
+
+	
+	
+
+//command villager, ex !villager maple
 	if (command === "villager") {
 		const villagerName = args[0];
-		let picExists = false;
-		let villagerImgSrc;
-		let villagerEmbed = new Discord.MessageEmbed().setTitle(villagerName);
-		const link2 = `https://animalcrossing.fandom.com/wiki/${villagerName}_(villager)`;
-		request(link2, (err, res, body) => {
-			if (err) {
-				return message.channel.send(`There was an error getting info on ${villagerName}!`);
-			}
-			const $ = cheerio.load(body);
-			const villagerImgSrcRaw = $('figure').first().find('img').attr('src');
-			console.log("first", villagerImgSrcRaw)
-			if (villagerImgSrcRaw === undefined) {
-				return;
-			} else {
-				picExists = true;
-				villagerImgSrc = villagerImgSrcRaw.split('/').slice(0,8).join('/');
-				villagerEmbed = villagerEmbed.setThumbnail(villagerImgSrc);
-				// console.log($('figure').first().find('img').attr('src'))
-				message.channel.send(villagerEmbed);
-			}
-		});
-		const link = `https://animalcrossing.fandom.com/wiki/${villagerName}`;
-		request(link, (err, res, body) => {
-			if (err) {
-				return message.channel.send(`There was an error getting info on ${villagerName}!`);
-			}
-			const $ = cheerio.load(body);
-			const villagerImgSrcRaw = $('figure').first().find('img').attr('src');
-			console.log("second", villagerImgSrcRaw)
-			if (villagerImgSrcRaw === undefined && !picExists) {
-				return message.channel.send(`No picture of ${villagerName}!`);
-			} else if (picExists) {
-				return;
-			} else {
-				picExists = true;
-				villagerImgSrc = villagerImgSrcRaw.split('/').slice(0,8).join('/');
-				console.log(villagerImgSrc)
-				villagerEmbed = villagerEmbed.setThumbnail(villagerImgSrc);
-				message.channel.send(villagerEmbed);
-			}
-		});
+		let showName = true
+		getVillagerPicture(villagerName, showName, message);
 	}
+
+	
+//villager list 
+	if(command === "guess"){
+		console.log(args[0])
+		villagerName = getRandomVillager().toLowerCase();
+		wrongAnswer = true;
+		console.log("Here")
+		console.log(villagerName);
+		let showName = false
+		getVillagerPicture(villagerName, showName, message);
+		guessOn = true;
+		
+		// while(wrongAnswer){
+		// 	if (args[0].toLowerCase() === villagerName){
+		// 		message.channel.send("that's correct!");
+		// 		wrongAnswer = false;
+		// 	}
+
+		// }
+
+		
+
+
+	}
+
+
 });
 
 client.login(config.token);
