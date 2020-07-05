653cd8e7848bed29d80e1f0f9f767a426e50bdd6
Kali Cornn <kcornn@stanford.edu>
2020-07-04 23:58:41 -0700

    used villagerdb for villager command for more consistent urls

--- commands/villager.js
+++ commands/villager.js
@@ -10,52 +10,26 @@ exports.run = (client, message, args) => {
 	getVillagerPicture(villagerName, showName, message);
 }
 
-// todo: figure out what villagers have different urls than the ones currently checked
-// ex: sally's URL: https://animalcrossing.fandom.com/wiki/Sally_(Animal_Crossing) - !villager sally doesn't work
 function getVillagerPicture(villagerName, showName, message){
-	let picExists = false;
-	let villagerImgSrc;
-	let villagerEmbed;
+	let villagerEmbed = new Discord.MessageEmbed()
 	if (showName) {
-		villagerEmbed = new Discord.MessageEmbed().setTitle(villagerName);
+		villagerEmbed.setTitle(villagerName);
 	} else {
-		villagerEmbed = new Discord.MessageEmbed().setTitle('??');
+		villagerEmbed.setTitle('??');
 	}
-	const link2 = `https://animalcrossing.fandom.com/wiki/${villagerName}_(villager)`;
-	request(link2, (err, res, body) => {
-		if (err) {
-			return message.channel.send(`There was an error getting info on ${villagerName}!`);
-		}
-		const $ = cheerio.load(body);
-		const villagerImgSrcRaw = $('figure').first().find('img').attr('src');
-		console.log("first", villagerImgSrcRaw)
-		if (villagerImgSrcRaw === undefined) {
-			return;
-		} else {
-			picExists = true;
-			villagerImgSrc = villagerImgSrcRaw.split('/').slice(0, 8).join('/');
-			villagerEmbed = villagerEmbed.setThumbnail(villagerImgSrc);
-			// console.log($('figure').first().find('img').attr('src'))
-			message.channel.send(villagerEmbed);
-		}
-	});
-	const link = `https://animalcrossing.fandom.com/wiki/${villagerName}`;
+	const link = villagerName.toLowerCase() === "sally" ? `https://villagerdb.com/villager/${villagerName}2` : `https://villagerdb.com/villager/${villagerName}`;
+	console.log(link)
 	request(link, (err, res, body) => {
 		if (err) {
 			return message.channel.send(`There was an error getting info on ${villagerName}!`);
 		}
 		const $ = cheerio.load(body);
-		const villagerImgSrcRaw = $('figure').first().find('img').attr('src');
-		console.log("second", villagerImgSrcRaw)
-		if (villagerImgSrcRaw === undefined && !picExists) {
+		if (!$('img').get(1)) {
 			return message.channel.send(`No picture of ${villagerName}!`);
-		} else if (picExists) {
-			return;
 		} else {
-			picExists = true;
-			villagerImgSrc = villagerImgSrcRaw.split('/').slice(0, 8).join('/');
-			console.log(villagerImgSrc)
-			villagerEmbed = villagerEmbed.setThumbnail(villagerImgSrc);
+			const villagerImgSrcRaw = $('img').get(1).attribs.src;
+			const villagerImgSrc = `https://villagerdb.com${villagerImgSrcRaw}`;
+			villagerEmbed.setThumbnail(villagerImgSrc);
 			message.channel.send(villagerEmbed);
 		}
 	});
